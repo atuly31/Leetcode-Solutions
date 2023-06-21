daa85e6cdb8f8064e9bf1fd2aab8f5706747382a
atuly31 <106878502+atuly31@users.noreply.github.com>
2023-06-21 21:44:04 +0530

    Time: 211 ms (11.27%), Space: 96.4 MB (82.24%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+       int c=0,d=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]){
+                c=c+1;
+            }
+        }
+       for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>=nums[i+1]){
+                d=d+1;
+            }
+        }  
+        if(c==nums.size()-1 || d==nums.size()-1)
+            return true;
+        else{
+            return false;
+        }
+    }
+};
