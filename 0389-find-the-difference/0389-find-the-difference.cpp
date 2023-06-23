48f1e00db6b79724df593ac3ae00961607e365cd
atuly31 <106878502+atuly31@users.noreply.github.com>
2023-06-23 14:18:22 +0530

    Time: 4 ms (60.47%), Space: 6.8 MB (22.23%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+     sort(s.begin(),s.end());
+     sort(t.begin(),t.end());
+        for(int i=0;i<t.size();i++){
+            if(s[i]==t[i]){
+                continue;
+            }
+            else{
+                return t[i];
+            }
+        }
+         return t[s.length()]; 
+    }
+};
