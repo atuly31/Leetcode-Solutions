07c72ed54905cf2f47fe775f167431e0a7372275
atuly31 <106878502+atuly31@users.noreply.github.com>
2023-07-29 17:06:42 +0530

    Time: 5 ms (79.19%), Space: 6.9 MB (57.12%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+      int hash[256]={0};
+      bool ischarmapped[256]={0};
+      for(int i=0;i<s.size();i++){
+          if(hash[s[i]]==0&& ischarmapped[t[i]]==0){
+              hash[s[i]]=t[i];
+              ischarmapped[t[i]]=true;
+          }
+      }
+       for(int i=0;i<s.size();i++){
+           if(char(hash[s[i]])!=t[i]){
+               return false;
+           }
+       }
+        return true;
+    }
+};
