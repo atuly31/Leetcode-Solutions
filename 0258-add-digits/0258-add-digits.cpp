068ba33efa601ad0e8d10f85254106d5f0417ec1
atuly31 <106878502+atuly31@users.noreply.github.com>
2023-09-09 11:12:27 +0530

    Time: 2 ms (41.81%), Space: 6.1 MB (13.06%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+
+    int addDigits(int num) {
+      int rem ;
+        int sum=0;
+        while(num!=0)
+    {
+        sum = sum + num%10;
+        num=num/10;
+     }
+     
+     if(sum<10) 
+      return sum;
+     else 
+       return addDigits(sum);
+     
+    }
+};
