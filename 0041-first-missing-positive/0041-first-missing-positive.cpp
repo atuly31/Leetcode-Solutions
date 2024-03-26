1e39ebb55d944b1c58d27565ff503ba395e555f3
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-03-26 22:00:01 +0530

    Time: 49 ms (35.8%), Space: 43.7 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
-        for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
-        for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
-        return n + 1;
+        int n= size(nums);
+       
+        for(int i=0;i<n;i++){
+            int x=nums[i]; // x = current element
+            
+        // x>=1 && x<=n : to check if x is in range[1, n]
+        // x != i+1 : skip if at index i correct element is present.
+        // nums[x-1]!=x: skip if at index x-1 correct element is present
+            while(x>=1 && x<=n && x!=i+1 && nums[x-1]!=x){
+                swap(nums[x-1],nums[i]);
+                x=nums[i];
+            }
+        }
+
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == i+1)continue;
+                return i+1;       
+            
+        }
+        
+        return n+1;
     }
 };
