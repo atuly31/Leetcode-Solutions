52d77f9a1cea3ad5b38b80110f2bd46780f7701b
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-04-17 20:52:34 +0530

    Time: 2 ms (42.6%), Space: 10.7 MB (15.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-         short int i = digits.size() - 1;
-        if (i >= 0) {
-            while (i != -1) {
-                digits[i] += 1;
-                if (i == 0 && digits[i] == 10) {
-                    digits[i] = 0;
-                    digits.insert(digits.begin(), 1);
-                }
-                else if (digits[i] == 10) digits[i] = 0;
-                else break;
-                i--;
-            }
-        }
-        return digits;
-    
-    
+    string str ="";
+    string str1 ="1";
+    for(int i=0;i<digits.size();i++) 
+    {
+       char ch = digits[i] +'0';
+       str = str +ch;
+    }
+    vector<int> ans;
+    int carry =0;
+   
+    int i = digits.size()-1,j=0;
+    while(i>=0 || j>=0)
+    { 
+       
+       int n1 = (i>=0 ? str[i]:'0')-'0';
+       int n2 = (j>=0 ? str1[j]:'0')-'0';
+       int sum = n1+n2+carry;
+       ans.push_back(sum%10);
+       carry = sum/10;
+       i--;
+       j--;
+    }
+    if (carry!=0)  ans.push_back(carry);
+    reverse(ans.begin(),ans.end());
+
+    return ans;
     }
 };
