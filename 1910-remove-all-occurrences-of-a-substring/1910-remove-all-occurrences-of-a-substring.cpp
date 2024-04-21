69edadbd281fa10ae1819a6e936b3c95787c536c
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-04-21 12:06:51 +0530

    Time: 3 ms (62.56%), Space: 8.6 MB (15.36%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+
+void remove_rec(string &s, string part,int n)
+{
+      if(s.find(part) == std::string ::npos) return ;
+        
+        size_t  idx = s.find(part);
+        s.erase(idx,n);
+
+        remove_rec(s,part,n);
+}
     string removeOccurrences(string s, string part) {
         
-       while(s.find(part) != string ::npos){
-        size_t  idx = s.find(part);
+       
         int n = part.size();
-        s.erase(idx,n);
-       } 
+        remove_rec(s,part,n);
+       
        return s;
     }
 };
