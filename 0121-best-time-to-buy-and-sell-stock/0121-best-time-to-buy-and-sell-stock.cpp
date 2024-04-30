660ff796eb9d4071fe69281b953743eaa4cd34aa
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-04-30 10:35:20 +0530

    Time: 93 ms (28.76%), Space: 95.9 MB (31.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
-         int maxi = INT_MIN;
+         int maxi = 0;
         int i=0,j=0;
         int stock_pur = p[0];
         if(p.size()<=1) return 0;
         while(j<p.size()-1)
         {
            
-           int diff =p[j+1]-stock_pur  ;
-           if(p[j+1]<stock_pur)
-           {
-            stock_pur = p[j+1];
-            cout<<stock_pur;
-           }
-            maxi = max(maxi,diff);
+       
+            maxi = max(maxi,p[j+1]-stock_pur);
+            stock_pur = min(p[j+1],stock_pur);
+            
            j++;
             
         }
-        if(maxi<0) return 0;
+        // if(maxi<0) return 0;
         return maxi;
     }
 };
