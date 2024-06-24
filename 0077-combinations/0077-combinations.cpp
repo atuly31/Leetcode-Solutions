60d7feef69de0ba83b96a39f23c5a4f632128c0e
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-06-24 17:11:27 +0530

    Time: 157 ms (26.45%), Space: 130.5 MB (21.7%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-   void combo(vector<vector<int>> &ans,vector<int>v,int n,int k,int i)
+  vector<vector<int>> ans;
+   void combo(vector<int>&v,int n,int k,int i)
    {
-    if(v.size()>k) return;
-    if(i>n)
+    //if(v.size()>k) return;
+    if(i>n || v.size()>k)
     {
         if(v.size()==k) ans.push_back(v);
         return; 
     }
     v.push_back(i);
-    combo(ans,v,n,k,i+1);
+    combo(v,n,k,i+1);
     v.pop_back();
-    combo(ans,v,n,k,i+1);
+    combo(v,n,k,i+1);
    }
     vector<vector<int>> combine(int n, int k) {
-            vector<vector<int>> ans;
+            
             vector<int> res;
-            combo(ans,res,n,k,1);
+            combo(res,n,k,1);
             return ans;
     }
 };
