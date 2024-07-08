e74cbd3ed1283c11de688012bb3a98a06059770a
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-07-08 12:46:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,52 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-         
-        for(int i=0;i<nums.size();i++){
-            if(target==nums[i]){
-                return i;
-            }
+    int pivot(vector<int>& nums)
+    {
+        int i =0,j=nums.size()-1;
+        while(i<j)
+        {
+            int mid = (i+j)/2;
+            if(nums[0]<=nums[mid]) i = mid+1;
+            else j = mid;
         }
-        return -1;
+        return j;
     }
+
+    int binary_search(vector<int>& arr, int target,int i,int j)
+    {
+       int ans =-1;
+      
+        while(i<=j)
+        {
+            int mid = (i+j)/2;
+            if(arr[mid]==target) 
+            {
+                ans = mid;
+                return ans;
+            }
+            else if(arr[mid]>target) j = mid-1;
+            else i = mid+1;
+        }
+        return ans;
+    }
+    int search(vector<int>& nums, int target) 
+    {
+        int piv = pivot(nums);
+        if(nums.size() == 1 && nums[0] == target){
+            return 0 ;
+        }
+        else if(nums.size() == 1 && nums[0] != target){
+            return -1;
+        }
+        if(target>=nums[0])
+        {
+            return binary_search(nums,target,0,piv-1);
+           
+        } 
+        else 
+        {
+          return  binary_search(nums,target,piv,nums.size()-1);
+        }
         
-    
+    }
 };
