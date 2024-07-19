21d04cf2cf6104dc937a9c64b6657b9f2cef5f19
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-07-19 18:50:20 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.46%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,40 +1,52 @@
 class MyQueue {
 public:
-    stack<int> st1,st2;
+   stack<int>st1,st2;
     MyQueue() {
         
     }
     
-    void push(int x) {
-      st1.push(x);  
+    void push(int x) 
+    {
+        st1.push(x);
     }
     
-    int pop() {
-    if(st2.empty()){
-        while(st1.empty()==false){
+    int pop() 
+    {
+        while(!st1.empty())
+        {
             st2.push(st1.top());
             st1.pop();
         }
-    }
-        int x=st2.top();
+        int n = st2.top();
         st2.pop();
-        return x;
+        while(!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return n;
     }
     
     int peek() {
-       if(st2.empty()){
-        while(!st1.empty()){
+        while(!st1.empty())
+        {
             st2.push(st1.top());
             st1.pop();
         }
+        int n = st2.top();
+        while(!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return n;
+
     }
-    return  st2.top();
     
-    }
-    
-    bool empty() {
-        if(st1.empty() && st2.empty()) return true;
-        else return false;
+    bool empty() 
+    {
+        if(st1.empty()) return true;
+        return false;
     }
 };
 
