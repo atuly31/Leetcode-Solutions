dfa2f6789b23353cd52cc717db76ab3142270dae
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-07-23 10:48:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string frequencySort(string s) 
+    {
+        map<char,int>mp;
+        for(auto i:s) mp[i]++;
+       auto cmp = [&](char a,char b)
+       {
+        return mp[a]>mp[b];
+       };
+        sort(s.begin(),s.end(),cmp);
+        return s;
+    }
+};
