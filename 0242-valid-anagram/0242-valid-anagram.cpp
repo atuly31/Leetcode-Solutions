1c787e9b410a12e145869f7f210fc7419108052f
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-07-27 22:56:40 +0530

    Time: 8 ms (63.56%), Space: 8.9 MB (40.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       int n = s.length();
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       if(s.compare(t)==0){
-           return true;
-       }
-      return false;
+        if(s.size()!=t.size()) return false;
+        int hash[256]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            hash[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            hash[t[i]]--;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+           if(hash[s[i]]!=0) return false;
+        }
+        return true;
     }
 };
