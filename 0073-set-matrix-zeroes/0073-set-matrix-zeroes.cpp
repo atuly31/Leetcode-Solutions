e8c01b607fd3f84f221ce18fe248745c971b3748
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-08-08 04:01:13 +0530

    Time: 8 ms (76.81%), Space: 17 MB (17.9%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-      set<int> r;
-      set<int> c;
-      int m = matrix.size();
-      int n = matrix[0].size();
-     for(int i=0;i<m;i++){
-         for(int j=0;j<n;j++){
-             if(matrix[i][j]==0) {
-              r.insert(i);    
-              c.insert(j);
-             }
-             }
-     }
-     for(int i=0;i<m;i++){
-         for(int j=0;j<n;j++){
-             if(r.count(i) || c.count(j)) {
-                matrix[i][j]=0; 
-             }
-             }
-     }
-        //matrix;
+    void setZeroes(vector<vector<int>>& mat) {
+        vector<pair<int,int>>p;
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0) p.push_back({i,j});
+                else continue;
+            }
+        }
+        for(auto i:p)
+        {
+            int row =i.first;
+            int col = i.second;
+            fill(mat[row].begin(),mat[row].end(),0);
+            for(int i=0;i<n;i++) mat[i][col]=0;
+        }
     }
 };
