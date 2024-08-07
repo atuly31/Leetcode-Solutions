994b78b926dce1822d368b3c347ad417dd181132
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-08-07 10:31:33 +0530

    Time: 6 ms (59.01%), Space: 15.1 MB (52.72%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-
+//  static bool cmp(int a ,int b)
+//  {
+//     if(mp[a]==mp[b]) return mp[a]>mp[b];
+//     return mp[a]<mp[b];
+//  }
     vector<int> frequencySort(vector<int>& nums) {
         map<int,int>mp;
-        for(auto i:nums) mp[i]++;
-       auto cmp = [&](int a,int b)
-       {
-      if(mp[a]==mp[b]) return a>b;
-      else return mp[a]<mp[b];
-      };
 
+        for(auto i:nums)mp[i]++;
+       auto cmp = [&](int a ,int b)
+       {
+          if(mp[a]==mp[b]) return a>b;
+           return mp[a]<mp[b];
+       };
         sort(nums.begin(),nums.end(),cmp);
         return nums;
-        
     }
 };
