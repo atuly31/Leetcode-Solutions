8e81266f5559f069b305798f5d3971ff5d5acac7
Atul Yadav <106878502+atuly31@users.noreply.github.com>
2024-09-08 12:43:18 +0530

    Time: 8 ms (63.89%), Space: 15.5 MB (11.75%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -10,38 +10,21 @@
  */
 class Solution {
 public:
-     int getlen(ListNode* head){
-         int len =0;
-         ListNode* temp = head;
-         while(temp){
-             temp = temp->next;
-             len++;
-         }
-         return len;
-     }
+
     ListNode* oddEvenList(ListNode* head) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* dummy1 = new ListNode(0);
-        ListNode* temp = dummy;
-        ListNode* temp1 = dummy1;
-        int index = 1;
-        while(head){
-            if(index % 2==1)
-            {
-            temp->next = new ListNode(head->val);
-            temp = temp->next;
-            }
-            else 
-            {
-            temp1->next = new ListNode(head->val);
-            temp1 = temp1->next;  
-            }
-            head = head->next;
-            index++;
+       
+        if(head == NULL || head->next==NULL || head->next->next==NULL) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode*evenhead = even;
+        while(odd->next!=NULL && even->next!=NULL)
+        {
+             odd->next = even->next;
+             odd = even->next;
+             even ->next  = odd->next;
+             even = odd->next;
         }
-            temp->next = dummy1->next;
-            temp1->next = nullptr;
-    
-       return dummy->next;
+        odd ->next = evenhead;
+        return head;
     }
 };
